572662b425dd3604aca7682e0674ad884203fbaa
Tolunay Güdük <guduktolunay@gmail.com>
2017-12-27 20:49:39 +0300

    fist commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var request = require('request'); // İstek yapmak için kullanılacak paket.
+var cheerio = require('cheerio'); // Getirilen veriyi parçalamak için kullanılacak paket.
+
+request('https://twitter.com/HopaliiN', function (error, response, html) {
+    if ( ! error && response.statusCode == 200) {
+
+        var $ = cheerio.load(html);
+
+        $('li.stream-item').each(function(i, element){
+
+            console.log($(this).find('p.tweet-text').text());
+        });
+    }
+});
